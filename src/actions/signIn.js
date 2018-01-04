6f6d007450ca4d6b49b6a9f5e4a70c843230675d
Gulmira Berdalieva <gulmira2410@gmail.com>
2018-01-04 14:03:47 -0800

    add error handling to signup and signin

--- src/actions/signIn.js
+++ src/actions/signIn.js
@@ -1,5 +1,5 @@
 import db from '../db'
 
 export default function signIn(email, password) {
-  return db.one('SELECT * FROM users WHERE email=$1, password=$2', [email, password])
+  return db.one('SELECT * FROM users WHERE email=$1 AND password=$2', [email, password])
 }
--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -8,7 +8,9 @@ import {
 const router = express.Router()
 
 router.get('/sign-up', (req, res) => {
-  res.render('authentication/sign-up')
+  res.render('authentication/sign-up', {
+    error: '',
+  })
 })
 
 router.post('/sign-up', (req, res, next) => {
@@ -18,13 +20,47 @@ router.post('/sign-up', (req, res, next) => {
     password,
   } = req.body
 
-  signUp(name, email, password)
-    .then(user => res.redirect('/'))
-    .catch(next)
+  if (!name || !email || !password) {
+    res.render('authentication/sign-up', {
+      error: 'All fields required!'
+    })
+  } else {
+    signUp(name, email, password)
+      .then(user => res.redirect('/'))
+      .catch(next)
+  }
 })
 
+
 router.get('/sign-in', (req, res) => {
-  res.render('authentication/sign-in')
+  res.render('authentication/sign-in', {
+    error: '',
+  })
 })
+
+
+router.post('/sign-in', (req, res, next) => {
+  const {
+    email,
+    password,
+  } = req.body
+
+  if (!email || !password) {
+    res.render('authentication/sign-in', {
+      error: 'Both fields required!',
+    })
+  } else {
+    signIn(email, password)
+      .then(() => {
+        res.redirect('/')
+      })
+      .catch((next) => {
+        res.render('authentication/sign-in', {
+          error: 'Wrong email or password.',
+        })
+      })
+  }
+})
+
 
 export default router
