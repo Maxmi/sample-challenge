d85b435d30693da252bc3bdab35dd4a2a8379a57
Mira <Maxmi@users.noreply.github.com>
2018-01-29 13:59:04 -0800

    Update signUp.test.js

--- test/actions/signUp.test.js
+++ test/actions/signUp.test.js
@@ -2,17 +2,9 @@ import '../env'
 import { expect } from 'chai';
 import { signUp } from '../../src/actions';
 
-const table = ['users']
-
-
 describe('function signUp ', () => {
-
   it('should create a new row in the users table', () => {
     // TODO: FIXME and write a real test
-    return signUp('test', 'test@test.com', '123')
-      .then(() => {
-        expect(table).to.have.length(1)
-      })
-      // .catch(console.error) - should not catch in tests!
+    expect(1).to.equal(2)
   })
 })
