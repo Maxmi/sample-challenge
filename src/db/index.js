55333c9b966851250f22b17d4521a3dc1ecc1fd2
Mira <Maxmi@users.noreply.github.com>
2018-01-29 13:24:11 -0800

    Update index.js

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,6 @@ import pg from 'pg-promise'
 
 const connect = pg({promiseLib: Promise})
 const db = connect(process.env.DATABASE_URL)
-//
-// const db = connect(process.env.NODE_ENV === 'test' ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL)
+
 
 export default db
