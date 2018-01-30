f45843ada5eeca63b39e003814150224d89cd31b
Mira <Maxmi@users.noreply.github.com>
2018-01-29 16:14:32 -0800

    Update albums.sql

--- src/data/albums.sql
+++ src/data/albums.sql
@@ -13,11 +13,3 @@ VALUES
   ('bob', 'bob@b.com', 'lol'),
   ('joe', 'joe@j.com', 'lol')
 ;
-
-
-INSERT INTO
-  reviews (user_id, album_id, title, body)
-VALUES
-  ('1', '1', 'bobs review title', 'bobs review'),
-  ('2', '2', 'joes review title', 'joes review')
-;
